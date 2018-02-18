42e5400ca96b68525a2f6ede63a7e057f8620b7e
Piotr Walkusz <piotrwalkusz1@gmail.com>
2018-02-18 19:19:54 +0100

    load-to-mongodb: allow specify the database name

--- scripts/load-to-mongodb.py
+++ scripts/load-to-mongodb.py
@@ -5,8 +5,8 @@ from gridfs import GridFS
 import sys
 
 client = MongoClient(sys.argv[1])
-fs = GridFS(client.lebrb)
-with open(sys.argv[2]) as f:
-    for i in fs.find({"filename": sys.argv[3]}):
+fs = GridFS(client[sys.argv[2]])
+with open(sys.argv[3]) as f:
+    for i in fs.find({"filename": sys.argv[4]}):
         fs.delete(i._id)
-    fs.put(f.read(), encoding="UTF-8", filename=sys.argv[3])
+    fs.put(f.read(), encoding="UTF-8", filename=sys.argv[4])
